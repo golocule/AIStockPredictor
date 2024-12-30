78e187fdb7b3c94d2707a33e30a635d6acdbd83f
golocule <129328765+golocule@users.noreply.github.com>
2024-12-29 23:10:41 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ exchanges = ccxt.exchanges
 exchange = ccxt.coinbaseadvanced()
 
 # code to gather candlestick data and store in a file (candles.npy)
-'''
+
 symbol = 'BTC/USD'
 timeframe = '5m'
 limit = 1000
@@ -25,7 +25,7 @@ stopDate = int(datetime(2024, 10, 1).timestamp() * 1000)
 
 #date to start gathering data
 currDate = int(datetime(2022, 2, 1).timestamp() * 1000)
-
+'''
 candleData = []
 
 while currDate < stopDate:
@@ -81,16 +81,21 @@ def maxNormalize(candleList, max):
 
         section = candleList[i]
         for j in range(6):
-            # sectMin = np.min(section[:, j])
-            # sectMax = np.max(section[:, j])
-
-            normalizedList[i, j] = (candleList[i, j] - min) / (max - min)
+            if j == 0:
+                normalizedList[i, j] = 0
+            else:
+                normalizedList[i, j] = (candleList[i, j] - min) / (max - min)
 
     normalizedList = normalizedList[0:(len(candleList - 1))]
     return normalizedList
 
 
 normalizedCandles = maxNormalize(historicalCandles, 130000)
+
+
+normCandle = normalizedCandles[163400]
+rawCandle = historicalCandles[163400]
+print("norm candle: ", normCandle, "raw candle: ", rawCandle)
 
 print("Normalized Candles...")
 
@@ -101,8 +106,8 @@ def dataPrep(candleData, input_length):
     for i in range(len(candleData) - input_length):
         currSequence = candleData[i:i + input_length]
         sequences.append(currSequence)
-
-        target = candleData[i + input_length][3]
+        # i think 4 is close
+        target = candleData[i + input_length][4]
         targets.append(target)
     print("Data Prepped...")
     return torch.tensor(sequences, dtype=torch.float32), torch.tensor(targets, dtype=torch.float32)
@@ -141,18 +146,23 @@ def createSequenceBatches(sequences, targets, batch_size):
         end = start + batch_size
         yield sequences[start:end], targets[start:end]
 
+device = torch.device('mps' if torch.backends.mps.is_available() else 'cpu')
+device = 'cpu'
+print(f'Using device: {device}')
 
-model = LSTMModel(input_size=6, hidden_size=64, output_size=1)
+model = LSTMModel(input_size=6, hidden_size=128, output_size=1).to(device)
 criterion = nn.MSELoss()
-optimizer = optim.Adam(model.parameters(), lr=0.01)
+optimizer = optim.Adam(model.parameters(), lr=1e-4)
 
 # mess with epochs (iterations through data) 
 # mess with batch_size (length segments of sequences)(better pattern recognition)
 epochs = 10
-batch_size = 32
+batch_size = 64
 
 # mess with input_length in this step:
-sequences, targets = dataPrep(historicalCandles, 50)
+sequences, targets = dataPrep(normalizedCandles, 50)
+
+
 
 print("Training model...")
 for epoch in range(epochs):
@@ -160,7 +170,7 @@ for epoch in range(epochs):
     epoch_loss = 0
 
     for sequenceBatch, targetBatch in createSequenceBatches(sequences, targets, batch_size):
-        sequenceBatch, targetBatch = sequenceBatch.clone(), targetBatch.clone()
+        sequenceBatch, targetBatch = sequenceBatch.clone().to(device), targetBatch.clone().to(device)
         optimizer.zero_grad()
         outputs = model(sequenceBatch)
         loss = criterion(outputs.squeeze(), targetBatch)
@@ -169,3 +179,18 @@ for epoch in range(epochs):
 
         epoch_loss += loss.item()
     print(f'Epoch {epoch} Loss {epoch_loss / len(sequences)}')
+
+
+newDate = int(datetime(2024, 12, 20).timestamp() * 1000)
+newCandles = exchange.fetch_ohlcv(symbol, timeframe, limit=50)
+newNormCandles = torch.tensor(maxNormalize(newCandles, 130000), dtype=torch.float32).unsqueeze(0).to(device)
+
+print(f'Last Candle Date: {datetime.utcfromtimestamp(newCandles[-1][0]/1000)} Last Cande Price: {newCandles[-1][4]}')
+
+model.eval()
+
+with torch.no_grad():
+    prediction = model(newNormCandles)
+
+prediction = prediction.squeeze().cpu()
+print("prediction:", prediction*130000)
